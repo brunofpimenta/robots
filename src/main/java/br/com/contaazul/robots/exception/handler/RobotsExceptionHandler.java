0178c43e69d09bfc5e1ef24e7de2a7df3f33da41
Bruno Pimenta <brunofpimenta@gmail.com>
2017-11-13 10:55:34 -0200

    Retornando a string "400 Bad Request" no caso de comando inválido ou posição inválida.

--- src/main/java/br/com/contaazul/robots/exception/handler/RobotsExceptionHandler.java
+++ src/main/java/br/com/contaazul/robots/exception/handler/RobotsExceptionHandler.java
@@ -2,16 +2,16 @@ package br.com.contaazul.robots.exception.handler;
 
 import br.com.contaazul.robots.exception.BusinessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class RobotsExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public void businessExceptionHandler() {
+    public ResponseEntity<String> businessExceptionHandler() {
+        return ResponseEntity.badRequest().body(String.format("%d %s", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase()));
     }
 
 }
